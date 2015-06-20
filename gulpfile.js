d7c70fdddd1a760a94c3f3787d5067436a2483cc
Gustavo de Geus <gustavodegeus@gmail.com>
2015-06-20 15:09:53 -0300

    dist tree

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var browserSync = require('browser-sync');
 
+var bases = {
+  app: 'app/',
+  dist: 'dist/',
+};
 var reload = browserSync.reload;
 
 // watch files for changes and reload
-gulp.task('serve', function() {
+gulp.task('serve', function () {
   browserSync({
     server: {
       baseDir: 'app',
@@ -13,6 +18,25 @@ gulp.task('serve', function() {
       }
     }
   });
-
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], { cwd: 'app' }, reload);
 });
+
+// Delete the dist directory
+gulp.task('clean', function() {
+ return gulp.src(bases.dist)
+ .pipe(clean());
+});
+
+gulp.task('app', function () {
+  return gulp.src(['app/**', '!app/{typings,typings/**}'])
+    .pipe(gulp.dest(bases.dist));
+});
+
+
+gulp.task('modules', function () {
+  return gulp.src('modules/**')
+    .pipe(gulp.dest('dist/modules'));
+});
+
+gulp.task('dist', ['clean', 'app', 'modules']);
+
