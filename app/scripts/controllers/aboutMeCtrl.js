6c7eaa35cfff83b1cdbf1a9a88cf83cdc32db341
Gustavo de Geus <gustavodegeus@gmail.com>
2015-06-21 22:30:10 -0300

    about me ctrl add

---
+++ app/scripts/controllers/aboutMeCtrl.js
@@ -0,0 +1,12 @@
+angular.module("personalPageApp").controller("aboutMeCtrl", function ($scope) {
+	
+	$scope.bioParagraphs = 	["Brazilian, born in Ponta Grossa-PR, I moved to Florianopolis-SC with 10 years. There I grew up and had a wonderful time in my life."
+	, "At 15 I remember my chosen profession, I wanted to work with information technology! My parents had an old 486 and I spent hours trying to find something to do with that contraption, without the help of internet yet."
+	, "Before work I've always been a fan of technology, video games, computers and smart devices always caught my attention."
+	, 'When the time came to choose the college course I did not have many problems and I chose to make Information Systems at UFSC. In college I came only with my "knowledge" as User and I had much difficulty in the first half to understand how it all worked. One topic that caught my attention was the "Software Development". The history of some code lines turn into something that could help people fascinated me from the start.'
+	, "As my course was evening, I had the privilege to work since the first months of college, starting on the support and finally as a programmer in 2010. Since then I work exclusively with software development, undergoing various types of companies and working with various types of pro"
+	, "After a few years working exclusively with Java language giving more focus to the backend, I began to study by myself frameworks and languages ​​that could give me greater autonomy during the development cycle. I started studying and developing some projects using Adobe Flex and in the last two years I started to study more deeply Java Script language and the main frameworks for development in that language."
+	, "How to claim for the future, I pretend to use all the knowledge acquired during the years to become a Solutions Architect and can help businesses find which technologies may be more fitting to the problems they are proposed to solve."
+	, "PS. In the photo, me, my wife Sarah and our dog Taurus! :)"];
+	
+});
