1e52d9518377f2a1efd90b01474c88c95e61cbda
Gustavo de Geus <gustavodegeus@gmail.com>
2015-06-22 18:41:56 -0300

    About me change

--- app/scripts/controllers/aboutMeCtrl.js
+++ app/scripts/controllers/aboutMeCtrl.js
@@ -1,7 +1,7 @@
 angular.module("personalPageApp").controller("aboutMeCtrl", function ($scope) {
 	
 	$scope.bioParagraphs = 	["Brazilian, born in Ponta Grossa-PR, Moved to Florianopolis-SC when I was 10 years old. I grew up there and had a wonderful time."
-	, "At 15, I remember discovering my chosen profession: information technology! My parents had an old 486 and I spent hours trying to find something to do with that hindrance, even without the help of internet."
+	, "At 15, I remember discovering my chosen profession: information technology! My parents had an old 486 and I spent hours trying to find something to do with that contraption, even without the help of internet."
 	, "Before working with IT, I've always been a fan of technology, video games, computers and smart devices."
 	, 'When the time came to choose a college course I did not have many problems and I chose to make Information Systems at UFSC. In college I came only with my "knowledge" as User and I had a lot of difficulty in the first half to understand how it all worked. One topic that caught my attention was the "Software Development". The idea of some lines of code turning into something that could help people fascinated me from the start.'
 	, "As my course was evening, I had the privilege of working since the first month of college, starting as support and finally as a programmer in 2010. Since then I've worked exclusively with software development with different types of companies and working with various types of professionals."
